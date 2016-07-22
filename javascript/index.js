69f145377c2e7379c62a3270ff7e905e206e8dd5
mirahmed39 <mir.ahmed39@yahoo.com>
2016-07-22 12:19:29 -0400

    initial commit

---
+++ javascript/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$("body").fadeIn(1000);
+});
+
+$(document).ready(function(){
+	$(".title").funnyText( {
+		speed: 350
+	});
+});
+
+$("#gallery a").touchTouch();
+
+
+
